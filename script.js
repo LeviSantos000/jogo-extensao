67a1bb63b4a7c7b947e12163b3ced61b8bbb4765
Pedro Henrique <l2ouroo@hotmail.com>
2025-09-10 23:17:25 -0300

    NEW FUNCTIONS, RULES OF THE GAME

--- script.js
+++ script.js
@@ -27,6 +27,43 @@ function iniciarJogo() {
 function abrirGabinete() {
     monitorgabinete.style.display = "none"
     gabineteAberto.style.display = "block"
+     
+    overlay.innerHTML = "<p id='pergunta'>A memória ___ é responsável por armazenar temporariamente os dados e as instruções que o computador utiliza para executar o sistema operacional, os programas e as tarefas <br></br> De que peça estámos falando ?</p>" 
+
+    let inputElement = document.createElement("input")
+    inputElement.type = "text"
+    inputElement.placeholder = "Digite aqui"
+    inputElement.id = "resposta"
+
+    let botaoResponder = document.createElement("button")
+    botaoResponder.textContent = "Responder"
+    botaoResponder.onclick = verificarResposta
+
+    let divMensagem = document.createElement("div")
+    divMensagem.id = "mensagem"
+
+    overlay.appendChild(inputElement)
+    overlay.appendChild(botaoResponder)
+    overlay.appendChild(divMensagem)
+    overlay.style.display="flex"
+
+}
+
+function verificarResposta(){
+    
+    let resposta = document.getElementById("resposta").value.toLowerCase()
+    let divMensagem = document.getElementById("mensagem")
+    divMensagem.innerHTML = "" 
+    
+    if (resposta.includes("ram")) {
+        divMensagem.innerHTML = "<p style='color:lightgreen'>🎉 Parabéns! Você acertou: o problema era na RAM.</p>"
+    }
+     else {
+      divMensagem.innerHTML = "<p style='color:red'>❌ Resposta errada. Tente novamente!</p>"
+
+    }
+
+    
 }
 
 function fecharGabinete() {
